eaf6c4bb8fda9c813b148150997130b5071a47e4
IL41ML <il41ml@stud.uni-obuda.hu>
2021-11-14 16:00:25 +0100

    Interface created for basic universal CRUD ops. -Create T entity, -Read one entity,  -Read all entity, - Remove T entity, -Remove by ID.

---
+++ IL41ML_HFT_2021221.Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL41ML_HFT_2021221.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetOne(int id);
+        IQueryable<T> GetAll();
+        void Insert(T entity);
+        void Remove(T entity);
+        void Remove(int id);
+    }
+}
