eb8dd3cf187ed653b4e97326a0737e355218456e
IL41ML <il41ml@stud.uni-obuda.hu>
2021-11-14 16:03:58 +0100

    Define entity Specific operations, Update ops.

---
+++ IL41ML_HFT_2021221.Repository/IServiceRepository.cs
@@ -0,0 +1,16 @@
+﻿using IL41ML_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL41ML_HFT_2021221.Repository
+{
+    public interface IServiceRepository : IRepository<Service>
+    {
+        void ChangeName(int id, string newName);
+        void ChangeWeb(int id, string newWeb);
+        void ChangePhoneNumber(int id, string newPhoneNumber);
+    }
+}
---
+++ IL41ML_HFT_2021221.Repository/IShopRepository.cs
@@ -0,0 +1,15 @@
+﻿using IL41ML_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL41ML_HFT_2021221.Repository
+{
+    public interface IShopRepository : IRepository<Shop>
+    {
+        void ChangePhoneNumber(int id, string newPhoneNumber);
+        void ChangeName(int id, string newName);
+    }
+}
