b1ce4e8796e91183262aff24335764cc26c62239
IL41ML <il41ml@stud.uni-obuda.hu>
2021-11-21 18:45:46 +0100

    Custom datatype for easier querries.

---
+++ IL41ML_HFT_2021221.Logic/DataType/INameAndDouble.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL41ML_HFT_2021221.Logic.DataType
+{
+    internal interface INameAndDouble
+    {
+        string Name { get; set; }
+        double Average { get; set; }
+    }
+}
---
+++ IL41ML_HFT_2021221.Logic/DataType/NameAndDouble.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL41ML_HFT_2021221.Logic.DataType
+{
+    public class NameAndDouble : INameAndDouble
+    {
+        public string Name { get; set; }
+        public double Average { get; set; }
+
+        public override string ToString()
+        {
+            return "{ "
+                + nameof(this.Name) + " = " + this.Name + " , "
+                + nameof(this.Average) + " = " + Math.Round(this.Average) + " HUF"
+                + " }";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is NameAndDouble)
+            {
+                NameAndDouble other = obj as NameAndDouble;
+                return this.Name == other.Name &&
+                    this.Average == other.Average;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+    }
+}
