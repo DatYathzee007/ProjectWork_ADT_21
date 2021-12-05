e3a142cecad55d5cd2cdbe88bf0a5a69974c4e5f
IL41ML <il41ml@stud.uni-obuda.hu>
2021-12-05 19:46:45 +0100

    services.AddSingleton<DbContext>(); modified to AddTransient because of Singleton DB

--- IL41ML_HFT_2021221.Endpoint/Startup.cs
+++ IL41ML_HFT_2021221.Endpoint/Startup.cs
@@ -3,15 +3,9 @@ using IL41ML_HFT_2021221.Logic;
 using IL41ML_HFT_2021221.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace IL41ML_HFT_2021221.Endpoint
 {
@@ -38,9 +32,9 @@ namespace IL41ML_HFT_2021221.Endpoint
             services.AddTransient<IModelRepository, ModelRepository>();
             services.AddTransient<IServiceRepository, ServiceRepository>();
             services.AddTransient<IShopRepository, ShopRepository>();
-
-
-            services.AddSingleton<DbContext, CustomDbContext>();
+            
+            services.AddTransient<DbContext, CustomDbContext>();
+            //services.AddSingleton<DbContext, CustomDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
