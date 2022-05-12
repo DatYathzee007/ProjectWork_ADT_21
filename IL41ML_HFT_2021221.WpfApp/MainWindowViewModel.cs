e76daefabc585b54105d1b8649d3d74c5eb002ad
IL41ML <il41ml@stud.uni-obuda.hu>
2022-05-12 21:59:54 +0200

    wpf building

---
+++ IL41ML_HFT_2021221.WpfApp/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace IL41ML_HFT_2021221.WpfApp
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        public RelayCommand ShowManager { get; set; }
+        public RelayCommand ShowStock { get; set; }
+        public RelayCommand ShowCustomer { get; set; }
+        public MainWindowViewModel()
+        {
+            ShowManager = new RelayCommand(() => {
+                _ = new ManagerWindow.ShowDialog();
+                
+            });
+        }
+    }
+}
