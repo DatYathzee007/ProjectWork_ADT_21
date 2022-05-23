e493b255013124669609d062c8d6f70f2877a49f
IL41ML <il41ml@stud.uni-obuda.hu>
2022-05-24 00:25:47 +0200

    Update() status: 405​ statusText: "Method Not Allowed"

--- IL41ML_HFT_2021221.JS_Client/wwwroot/script.js
+++ IL41ML_HFT_2021221.JS_Client/wwwroot/script.js
@@ -56,7 +56,7 @@ function display() {
     brands.forEach(t => {
         document.getElementById('Brandresultarea').innerHTML +=
             "<tr><td>" + t.id + "</td><td>" + t.name + "</td><td>" + t.country + "</td><td>" + t.ceo + "</td><td>" + t.source + "</td><td>" + t.foundation + "</td><td>" +
-        `<button type="button" onclick="remove(${t.id})">Delete</button>`
+        `<button type="button" onclick="remove(${t.id})">Delete</button>` + `<button type="button" onclick="Update(${t.id})">Update</button>`
             +"</td></tr>";
     });
 }
@@ -98,5 +98,24 @@ function create() {
             getdata();
         })
         .catch((error) => { console.error('Error:', error); });
-        
+}
+function Update(id) {
+    console.log("Update started.");
+    let name = document.getElementById('brandname').value;
+    let country = document.getElementById('brandcountry').value;
+    let ceo = document.getElementById('brandceo').value;
+    let source = document.getElementById('brandsource').value;
+    console.log(name, country, ceo, source);
+    fetch('http://localhost:20347/manager/UpdateBrand', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { Id: id, Name: name, Country: country, Ceo: ceo, Source: source })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
 }
